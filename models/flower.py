f3f65d39ceace039ab4c010602bc01213982676d
omar99emad <omar.emad@coreline.tech>
2023-06-08 14:49:26 +0300

    init commit

---
+++ models/flower.py
@@ -0,0 +1,10 @@
+from odoo import models, fields
+class AModel(models.Model):
+    _name = 'flower.flower'
+
+    name = fields.Char("Common Name")
+    scientific_name = fields.Char("Scientific Name")
+    season_start = fields.Date("Season Start Date")
+    season_end = fields.Date("Season End Date")
+    watering_frequency = fields.Integer(help="Frequency is in number of days")
+    watering_amount = fields.Float("watering Amount(ml)")
